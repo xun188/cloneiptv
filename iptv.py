ce7455cd1fbbef6943876cf83cb93ce8020a255e
lujun88888 <59165882+lujun88888@users.noreply.github.com>
2021-03-28 16:12:43 +0800

    Create iptv.py
    iptv生成文件

---
+++ iptv.py
@@ -0,0 +1,8 @@
+import requests
+import base64
+from urllib import parse
+response = requests.get("https://luotuo.oss-cn-beijing.aliyuncs.com/list.txt")
+response = str(base64.b64decode(response.text), "utf8")
+with open("iptv.txt", "w", encoding="utf8") as f:
+    iptv_list = parse.unquote(response).split("\n\n")
+    f.write(parse.unquote("\n\n".join(iptv_list[:-2])))
