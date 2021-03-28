fe7890acb5f51ea733ecd2d706e6c0a5864380bb
lujun88888 <59165882+lujun88888@users.noreply.github.com>
2021-03-28 16:13:39 +0800

    iptv生成

--- iptv.py
+++ iptv.py
@@ -6,3 +6,6 @@ response = str(base64.b64decode(response.text), "utf8")
 with open("iptv.txt", "w", encoding="utf8") as f:
     iptv_list = parse.unquote(response).split("\n\n")
     f.write(parse.unquote("\n\n".join(iptv_list[:-2])))
+
+    
+    
